7207122d2676a87fced03d823a1a3054ca9d4ac4
Nandini1217 <nandini.solanki1217@gmail.com>
2022-04-26 19:18:03 +0530

    sum

--- 23Feb/sumOfn.java
+++ 23Feb/sumOfn.java
@@ -3,12 +3,18 @@ public class Main{
     public static void main(String[] args){
     Scanner scn=new Scanner(System.in);
     int num=scn.nextInt();
-    int sum=0;
-    for(int i=1;i<=num;i++){
-        sum=sum+i;
+//     int sum=0;
+//     for(int i=1;i<=num;i++){
+//         sum=sum+i;
         
-    }
-System.out.println(sum);
+//     }
+// System.out.println(sum);
+
+//or else the better option would have been to directly apply the formula 
+//becoz that would help to remove the loop also thus reducing the time complexity
+
+int res=(num*(num+1))/2;
+System.out.println(res);
 
     }
 }
