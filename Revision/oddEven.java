9a877ab342a66402fba2784b0b1fa38ccb6734cb
Nandini1217 <nandini.solanki1217@gmail.com>
2022-06-04 14:39:37 +0530

    rev

---
+++ Revision/oddEven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        if(n%2==0){
+            System.out.println("even no.");
+        }else{
+            System.out.println("odd no.");
+        }
+    }
+}
